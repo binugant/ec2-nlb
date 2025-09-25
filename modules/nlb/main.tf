fddac510028d2d41ad7a6531f46e70158c844575
Bhaskar Inuganti <44278791+binugant@users.noreply.github.com>
2025-09-25 15:40:46 -0500

    Update main.tf

--- modules/nlb/main.tf
+++ modules/nlb/main.tf
@@ -30,3 +30,34 @@ resource "aws_lb_target_group_attachment" "this" {
   target_id        = var.target_ids[count.index]
   port             = var.target_port
 }
+
+
+#######
+resource "aws_lb" "this" {
+  name               = var.nlb_name
+  internal           = var.internal
+  load_balancer_type = "network"
+  subnets            = var.subnets
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "this" {
+  name        = var.target_group_name
+  port        = var.target_group_port
+  protocol    = var.target_group_protocol
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
