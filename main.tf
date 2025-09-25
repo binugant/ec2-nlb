bf543ec25b30a75cf05606abcca08c52c5d82525
Bhaskar Inuganti <44278791+binugant@users.noreply.github.com>
2025-09-25 15:42:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,34 @@ module "nlb" {
   listener_port = 22
   target_port   = 22
 }
+
+
+###########
+module "nlb" {
+  source = "./modules/nlb"
+
+  nlb_name            = "web-nlb"
+  internal            = false
+  subnets             = var.public_subnets
+  vpc_id              = var.vpc_id
+  target_group_name   = "web-tg"
+  target_group_port   = 80
+  target_group_protocol = "TCP"
+  listener_port       = 80
+  listener_protocol   = "TCP"
+}
+
+resource "aws_autoscaling_group" "web_asg" {
+  vpc_zone_identifier = var.private_subnets
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = "$Latest"
+  }
+
+  # ✅ Attach to NLB Target Group from module
+  target_group_arns = [module.nlb.target_group_arn]
+}
