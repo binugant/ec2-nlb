ee1b03281aa56ed76196c300499587117d3aa9cc
Bhaskar Inuganti <44278791+binugant@users.noreply.github.com>
2025-09-25 15:41:43 -0500

    Update outputs.tf

--- modules/nlb/outputs.tf
+++ modules/nlb/outputs.tf
@@ -1,3 +1,15 @@
 output "dns_name" {
   value = aws_lb.this.dns_name
 }
+#########
+output "nlb_arn" {
+  value = aws_lb.this.arn
+}
+
+output "nlb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.this.arn
+}
